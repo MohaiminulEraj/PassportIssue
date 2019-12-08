07ebce4a987cda39131d00adba139e893a4dc71c
Nabil Akhunjee <nbakhunjee97@gmail.com>
2019-12-08 20:00:58 +0600

    Revert "Merge branch 'master' of https://github.com/Nabil160/PassportIssue"
    This reverts commit e76230fdb16cbaa3943f8ce5eddce998c81c5731, reversing
    changes made to 1dbd59ff55a4b13bd51732817425a189d619e026.

---
+++ src/mainpkg/MainPageController.java
@@ -0,0 +1,198 @@
+package mainpkg;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class MainPageController implements Initializable {
+
+    @FXML
+    private TextField userNameTextField;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Label forgotAccountLabel;
+    @FXML
+    private Label signUpAsCitizenLabel;
+    @FXML
+    private PasswordField loginPasswordField;
+    @FXML
+    private ComboBox userTypeComboBoxLogin;
+    
+    @Override 
+    public void initialize(URL url, ResourceBundle rb) {
+        userTypeComboBoxLogin.getItems().addAll("Admin","Home Ministry", "Citizen","Employee","Police");
+    }    
+    
+    @FXML
+    private void userNameTextField(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void loginButtonOnClick(ActionEvent event)throws IOException {
+    String uN = userNameTextField.getText();
+    String uP = loginPasswordField.getText();
+    String uG = userTypeComboBoxLogin.getValue().toString();
+        boolean found = false;
+        String tempUsername ="";
+        String tempPassword ="";
+        String tempGroup="";
+        File f = null; // new File("loginInfo.bin");
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        DataInputStream dis = null;
+        
+                //Scanner sc=new Scanner (f);
+                
+        try{
+
+            f = new File("loginInfo.bin");
+            String str="";
+            if(!f.exists()){
+                System.out.println("file nai :3");
+            }
+            
+            else{
+                
+                fis = new FileInputStream("loginInfo.bin");
+                //bis = new BufferedInputStream(fis);
+                dis = new DataInputStream(fis);
+                
+                
+                while(true){
+                    str+= dis.readUTF()
+                         +dis.readUTF()
+                         +dis.readUTF();
+                }
+            }
+            String[] user = str.split("\\??");
+            
+            for(int i=0; i<= (user.length-3); i=i+3){
+            System.out.println("User: "+ user[i]);
+                if(uN == user[i+1] && uP == user[i+2] && uG  == user[i+3]){
+            
+                    FXMLLoader loader = new FXMLLoader();
+                    loader.setLocation(getClass().getResource("CitizenHomeScene.fxml"));
+                    //Parent personViewParent = loader.load();
+                    Parent nextnewGUI = loader.load(); //FXMLLoader.load(getClass().getResource("CitizenHomeScene.fxml"));
+            
+                    Scene newScene = new Scene(nextnewGUI);
+                    Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+                    window.setScene(newScene);
+                    window.show();
+                    //found = true;
+                }
+                
+                if(uN.equals("Nabil") && uP.equals("Eraj") && uG.equals("Admin")){
+                    Parent nextnewGUI = FXMLLoader.load(getClass().getResource("AdminHomeScene.fxml"));
+
+                    Scene newScene = new Scene(nextnewGUI);
+
+                    Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+                    window.setScene(newScene);
+                    window.show();
+                }
+            }
+            /*sc.useDelimiter(" ");           // Space diya separate krtesi
+            while(sc.hasNextLine() && !found){
+            tempUsername = sc.next();
+            tempPassword = sc.next();
+            tempGroup = sc.next();
+            //if(tempUsername.trim().equals(uN.trim()) && tempPassword.trim().equals(Up.trim())
+            //if(tempUsername.trim().equals(uN.trim()) && tempPassword.trim().equals(uP.trim()) && userTypeComboBoxLogin.getValue().toString().equals("Citizen")){
+            if(uN.equals(tempUsername) && uP.equals(tempPassword) && userTypeComboBoxLogin.getValue().toString().equals("Citizen")){
+            
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("CitizenHomeScene.fxml"));
+            //Parent personViewParent = loader.load();
+            Parent nextnewGUI = loader.load(); //FXMLLoader.load(getClass().getResource("CitizenHomeScene.fxml"));
+            
+            Scene newScene = new Scene(nextnewGUI);
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(newScene);
+            window.show();
+            found = true;
+            }
+            
+            if(uN.equals(tempUsername) && uP.equals(tempPassword) && userTypeComboBoxLogin.getValue().toString().equals("Police")){
+            
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("PoliceHomeScene.fxml"));
+            //Parent personViewParent = loader.load();
+            Parent nextnewGUI = loader.load(); //FXMLLoader.load(getClass().getResource("CitizenHomeScene.fxml"));
+            
+            Scene newScene = new Scene(nextnewGUI);
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(newScene);
+            window.show();
+            found = true;
+            }
+            
+            }*/
+            
+            //sc.close();
+            } catch(Exception e){
+                System.out.println(e);
+            } finally{
+            try{
+                if(dis != null) dis.close();
+            } catch(Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+
+    @FXML
+    private void forgotAccountLabelOnClick(MouseEvent event) throws IOException {
+        Parent forgotAccountPopup = FXMLLoader.load(getClass().getResource("ForgotAccntpopup.fxml"));
+        Scene popupForgotAccountScene = new Scene(forgotAccountPopup);
+        
+        Stage window  = new Stage();
+        window.setTitle("Pop-up window Forgot Account");
+        window.setScene(popupForgotAccountScene);
+        window.show();
+    }
+
+    @FXML
+    private void signUpAsCitizenLabelOnClick(MouseEvent event) throws IOException {
+        Parent signupCitizenParent = FXMLLoader.load(getClass().getResource("SignupCitizenScene.fxml"));
+        Scene signupCitizenScene = new Scene(signupCitizenParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(signupCitizenScene);
+        window.show();
+    }
+    
+    /* public void changSceneOnUserType(String userHomeScene) throws IOException{
+    Parent nextnewGUI = FXMLLoader.load(getClass().getResource(userHomeScene));
+    
+    Scene newScene = new Scene(nextnewGUI);
+    
+    Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+    window.setScene(newScene);
+    window.show();
+    }*/
+    
+}
---
+++ src/mainpkg/SignupCitizenSceneController.java
@@ -0,0 +1,270 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainpkg;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Nabil Akhunjee
+ */
+public class SignupCitizenSceneController implements Initializable {
+
+    
+    @FXML
+    private TextField citizenFullNameTextField;
+    @FXML
+    private ToggleGroup gender;
+    @FXML
+    private TextField nationalIdTextField;
+    @FXML
+    private TextField heightTextField;
+    @FXML
+    private TextField citizenEmailTextField;
+    @FXML
+    private TextField citizenPhoneNoTextField;
+    
+    @FXML
+    private TextField citizenRelationGuadianTextField;
+    @FXML
+    private TextField citizenGuardianFullNameTextField;
+    @FXML
+    private ToggleGroup guardianNationality;
+    @FXML
+    private TextField citizenGuardianPhoneNoTextField;
+    @FXML
+    private TextField citizenGuardianEmailTextField;
+    @FXML
+    private TextField citizenPresentAddrVill;
+    @FXML
+    private TextField citizenPresentAddrRoad;
+    @FXML
+    private TextField citizenPresentAddrDistrict;
+    @FXML
+    private TextField citizenPresentAddrPO;
+    @FXML
+    private TextField citizenPresentAddrZip;
+    @FXML
+    private CheckBox isPermanentAddrSame;
+    @FXML
+    private TextField citizenPermanentAddrVill;
+    @FXML
+    private TextField citizenPermanentAddrRoad;
+    @FXML
+    private TextField citizenPermanentAddrDistrict;
+    @FXML
+    private TextField citizenPermanentAddrPO;
+    @FXML
+    private TextField citizenPermanentAddrZip;
+    @FXML
+    private CheckBox isDualCitizen;
+    @FXML
+    private TextField secondaryCountryTextField;
+    @FXML
+    private Button citizenSignupButton;
+    @FXML
+    private Button backToMainButton;
+    @FXML
+    private PasswordField citizenSignunpasswordField;
+    @FXML
+    private DatePicker citizenDobDatePicker;
+    @FXML
+    private ChoiceBox citizenReligionChoiceBox;
+    @FXML
+    private ChoiceBox guardianReligionChoiceBox;
+    @FXML
+    private Label warningLabel;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       citizenReligionChoiceBox.getItems().addAll("Islam","Hinduism","Buddhism","Christianity");
+       guardianReligionChoiceBox.getItems().addAll("Islam","Hinduism","Buddhism","Christianity");
+       
+       secondaryCountryTextField.setDisable(true);
+       
+    }    
+
+    
+    @FXML
+    private void citizenSignupButtonOnClick(ActionEvent event) throws IOException {
+        
+        boolean signupable = false;
+        warningLabel.setText("Please fill up the fields!");
+        
+        File f = null;
+        FileWriter fw = null; 
+        
+        File fLoginInfo = null;
+        FileWriter fwLoginInfo = null;
+        
+        File fBin = null;
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        DataOutputStream dos = null;
+        
+        RadioButton selectedRadioButton = (RadioButton) gender.getSelectedToggle();
+        String toogleGenderValue = selectedRadioButton.getText();
+        String guardianNationality = selectedRadioButton.getText();
+        
+        LocalDate localDate = citizenDobDatePicker.getValue();
+        String cEmail = citizenEmailTextField.getText();
+        String cPass;
+        
+        String[] atS = cEmail.split("@");
+        String[] dotS = cEmail.split("\\.");
+        for(int i=0; i<dotS.length; i++){
+            if(atS.length>1 && dotS.length>1){
+                cPass = citizenSignunpasswordField.getText();
+                if(!cPass.isEmpty()){
+                signupable = true;
+                } 
+                else {
+                    warningLabel.setText("Password Required!!!");
+                }
+            }
+            else {
+            warningLabel.setText("Invalid Email Address!!!");
+        }
+        }
+        if(signupable == true){
+        try {
+            f = new File("CitizenSignUpForm.txt");
+            if(f.exists()) fw = new FileWriter(f, true);
+            else fw = new FileWriter(f);
+            
+            fw.write(
+            	"Personal Information: "+ citizenFullNameTextField.getText()+" "
+                    +toogleGenderValue+" "
+                    +localDate+" "
+                    +nationalIdTextField.getText()+" "
+                    +heightTextField.getText()+" "
+                    +citizenReligionChoiceBox.getValue()+" "
+                    +citizenPhoneNoTextField.getText()+" "
+                    +citizenEmailTextField.getText()+
+                    "Present Address: "
+                    +citizenPresentAddrVill.getText()+" "
+                    +citizenPresentAddrRoad.getText()+" "
+                    +citizenPresentAddrDistrict.getText()+" "
+                    +citizenPresentAddrPO.getText()+" "
+                    +citizenPresentAddrZip.getText()+
+                    "Parmanent Address: "
+                    +citizenPermanentAddrVill.getText()+" "
+                    +citizenPermanentAddrRoad.getText()+" "
+                    +citizenPermanentAddrDistrict.getText()+" "
+                    +citizenPermanentAddrPO.getText()+" "
+                    +citizenPermanentAddrZip.getText()+
+                    "Guardian Information: "
+                    +citizenRelationGuadianTextField.getText()+" "
+                    +citizenGuardianFullNameTextField.getText()+" "
+                    +guardianNationality+" "
+                    +guardianReligionChoiceBox.getValue()+" "
+                    +citizenGuardianPhoneNoTextField.getText()+" "
+                    +citizenGuardianEmailTextField.getText()+" "+
+                    " Citizenship Information: "
+                    +secondaryCountryTextField.getText()+" "
+            );
+            
+            fLoginInfo = new File("LoginInfo.txt");
+            if(fLoginInfo.exists()) fwLoginInfo = new FileWriter(fLoginInfo, true);
+            else fwLoginInfo = new FileWriter(fLoginInfo);
+            
+            fwLoginInfo.write(
+                    citizenEmailTextField.getText()+" "+ citizenSignunpasswordField.getText() + " Citizen "
+            );
+            
+            fBin = new File("loginInfo.bin");
+            if(fBin.exists()) fos = new FileOutputStream(fBin,true);
+            else fos = new FileOutputStream(fBin);
+            
+            bos = new BufferedOutputStream(fos);
+            dos = new DataOutputStream(bos);
+            
+            dos.writeUTF(citizenEmailTextField.getText());
+            dos.writeUTF(citizenSignunpasswordField.getText());
+            dos.writeUTF("Citizen");
+            //dos.writeUTF(citizenSignunpasswordField.getText());
+            //dos.writeUTF("Citizen ");
+                 
+        } catch (IOException ex) {
+            Logger.getLogger(SignupCitizenSceneController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if(fw != null) fw.close();
+                if(fwLoginInfo != null) fwLoginInfo.close();
+                if(dos != null) dos.close();
+            } catch (IOException ex) {
+                Logger.getLogger(SignupCitizenSceneController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        Parent mainPageParent = FXMLLoader.load(getClass().getResource("CitizenHomeScene.fxml"));
+        Scene mainPageScene = new Scene(mainPageParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(mainPageScene);
+        window.show();
+     }   
+    }
+
+    @FXML
+    private void backToMainButtonOnClick(ActionEvent event) throws IOException {
+        Parent mainPageParent = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        Scene mainPageScene = new Scene(mainPageParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(mainPageScene);
+        window.show();
+    }
+
+    @FXML
+    private void isPermanentAddrSameSelected(ActionEvent event) {
+        if(isPermanentAddrSame.isSelected()){
+                    citizenPermanentAddrVill.setText(citizenPresentAddrVill.getText());
+                    citizenPermanentAddrRoad.setText(citizenPresentAddrRoad.getText());
+                    citizenPermanentAddrDistrict.setText(citizenPresentAddrDistrict.getText());
+                    citizenPermanentAddrPO.setText(citizenPresentAddrPO.getText());
+                    citizenPermanentAddrZip.setText(citizenPresentAddrZip.getText());
+                         
+        }  
+    }
+
+    @FXML
+    private void isDualCitizenSelected(ActionEvent event) {
+        if(isDualCitizen.isSelected())
+            secondaryCountryTextField.setDisable(false);
+        else
+            secondaryCountryTextField.setDisable(true);
+    }
+    
+}
