7314f870d57bf12f728674e3164d939fba8ebdfc
Nabil Akhunjee <nbakhunjee97@gmail.com>
2019-12-09 02:49:30 +0600

    chart added
    -pie chart
    -bar chart
    -line chart

--- src/mainpkg/CreateBarChartController.java
+++ src/mainpkg/CreateBarChartController.java
@@ -7,7 +7,20 @@ package mainpkg;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -16,6 +29,25 @@ import javafx.fxml.Initializable;
  */
 public class CreateBarChartController implements Initializable {
 
+    @FXML
+    private RadioButton genderRB;
+    @FXML
+    private ToggleGroup chart;
+    @FXML
+    private RadioButton religionRB;
+    @FXML
+    private RadioButton divisionRB;
+    @FXML
+    private Button LoadChartButton;
+    @FXML
+    private BarChart<String, Number> barChart;
+    @FXML
+    private Label statLabel;
+    @FXML
+    private NumberAxis yAxis;
+    @FXML
+    private CategoryAxis xAxis;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +55,58 @@ public class CreateBarChartController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void LoadChartButtonOnClick(ActionEvent event) {
+        if(genderRB.isSelected()){
+            XYChart.Series<String,Number> series = new XYChart.Series<String,Number>();
+            series.getData().add(new XYChart.Data<String,Number>("Male",90));
+            series.getData().add(new XYChart.Data<String,Number>("Female",50));
+            series.setName("Gender");
+            barChart.getData().clear();
+            barChart.getData().addAll(series);
+        }
+        else if(religionRB.isSelected()){
+            
+            //barChart.getData().addAll( series1 );
+            XYChart.Series<String,Number> series1 = new XYChart.Series<String,Number>();
+            series1.getData().add(new XYChart.Data<String,Number>("Islam",70));
+            series1.getData().add(new XYChart.Data<String,Number>("Hinduism",59));
+            series1.getData().add(new XYChart.Data<String,Number>("Buddhism",33));
+            series1.getData().add(new XYChart.Data<String,Number>("Christianity",9));
+            series1.setName("Religion");
+            barChart.getData().clear();
+            barChart.getData().addAll(series1);
+        }
+        else if(divisionRB.isSelected()){
+            XYChart.Series<String,Number> series2 = new XYChart.Series<String,Number>();
+            series2.getData().add(new XYChart.Data<String,Number>("Barisal",25));
+            series2.getData().add(new XYChart.Data<String,Number>("Chittagong",60));
+            series2.getData().add(new XYChart.Data<String,Number>("Dhaka",75));
+            series2.getData().add(new XYChart.Data<String,Number>("Khulna",55));
+            series2.getData().add(new XYChart.Data<String,Number>("Mymensingh",20));
+            series2.getData().add(new XYChart.Data<String,Number>("Rajshahi",40));
+            series2.getData().add(new XYChart.Data<String,Number>("Rangpur",19));
+            series2.getData().add(new XYChart.Data<String,Number>("Sylhet",22));
+            series2.setName("Division");
+            barChart.getData().clear();
+            barChart.getData().addAll(series2);
+        }
+        
+        
+    }
     
+    private void showDataOnMouseClick(){
+        for(XYChart.Series<String, Number> data: barChart.getData()){
+            data.getNode().addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>(){
+                @Override
+                public void handle(MouseEvent event) {
+                    statLabel.setText(String.valueOf(data.getChart())); 
+                }
+            }
+            );
+        }
+    }
 }
+ 
+    
--- src/mainpkg/CreateLineChartController.java
+++ src/mainpkg/CreateLineChartController.java
@@ -7,7 +7,15 @@ package mainpkg;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
@@ -16,6 +24,23 @@ import javafx.fxml.Initializable;
  */
 public class CreateLineChartController implements Initializable {
 
+    @FXML
+    private Button LoadChartButton;
+    @FXML
+    private LineChart<String, Number> lineChart;
+    @FXML
+    private RadioButton genderRB;
+    @FXML
+    private ToggleGroup chart;
+    @FXML
+    private RadioButton religionRB;
+    @FXML
+    private RadioButton divisionRB;
+    @FXML
+    private RadioButton addallRB;
+    @FXML
+    private ToggleGroup chart1;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +48,62 @@ public class CreateLineChartController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    XYChart.Series<String,Number> series = new XYChart.Series<String,Number>();
+    XYChart.Series<String,Number> series2 = new XYChart.Series<String,Number>();
+    XYChart.Series<String,Number> series3 = new XYChart.Series<String,Number>();
+        
+    @FXML
+    private void LoadChartButtonOnClick(ActionEvent event) {
+        
+        if(genderRB.isSelected()){
+            lineChart.getData().clear();
+            loadGenderChart();
+            lineChart.getData().add(series);
+        }
+        if(religionRB.isSelected()){
+            lineChart.getData().clear();
+            loadReligionChart();
+            lineChart.getData().add(series2);
+        }
+        if(divisionRB.isSelected()){
+            lineChart.getData().clear();
+            loadDivisionChart();
+            lineChart.getData().add(series3);
+        }
+        if(addallRB.isSelected()){
+            lineChart.getData().clear();
+            loadGenderChart();
+            loadReligionChart();
+            loadDivisionChart();
+            lineChart.getData().addAll(series,series2,series3);
+        }
+       
+        
+    }
+    
+    private void loadGenderChart(){
+        series.getData().add(new XYChart.Data<String,Number>("Male",200));
+        series.getData().add(new XYChart.Data<String,Number>("Female",500));
+        series.setName("Gender");
+    }
+    private void loadReligionChart(){
+        series2.getData().add(new XYChart.Data<String,Number>("Islam",70));
+        series2.getData().add(new XYChart.Data<String,Number>("Hinduism",59));
+        series2.getData().add(new XYChart.Data<String,Number>("Buddhism",33));
+        series2.getData().add(new XYChart.Data<String,Number>("Christianity",9));
+        series2.setName("Religion");
+    }
+    private void loadDivisionChart(){
+        series3.getData().add(new XYChart.Data<String,Number>("Barisal",25));
+        series3.getData().add(new XYChart.Data<String,Number>("Chittagong",60));
+        series3.getData().add(new XYChart.Data<String,Number>("Dhaka",75));
+        series3.getData().add(new XYChart.Data<String,Number>("Khulna",55));
+        series3.getData().add(new XYChart.Data<String,Number>("Mymensingh",20));
+        series3.getData().add(new XYChart.Data<String,Number>("Rajshahi",40));
+        series3.getData().add(new XYChart.Data<String,Number>("Rangpur",19));
+        series3.getData().add(new XYChart.Data<String,Number>("Sylhet",22));
+        series3.setName("Division");
+    }
     
 }
